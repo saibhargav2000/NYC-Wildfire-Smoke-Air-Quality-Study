be4786ed0aaa5c6a1c16ae36cc384541ea763bb0
sai bhargav <56105516+saibhargav2000@users.noreply.github.com>
2026-03-05 20:20:57 -0500

    Delete NYC-Air-Pollution-Analysis directory

--- NYC-Air-Pollution-Analysis/database/pollution_analysis.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT SUM(NULLIF(a.aerosol_value,'NaN'))/COUNT(a.aerosol_value) AS avg_aerosol
-FROM aerosol_pixels a
-JOIN parks p
-ON ST_Intersects(a.geom, p.geom);
--- NYC-Air-Pollution-Analysis/database/raster_processing.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE TABLE aerosol_pixels AS
-SELECT p.val AS aerosol_value, p.geom
-FROM aerosol_raster r
-CROSS JOIN LATERAL ST_DumpAsPolygons(r.rast) AS p;
